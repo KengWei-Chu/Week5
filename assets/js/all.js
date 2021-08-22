83dc75b40fc232e521fce20914263a19075d3ca9
Va1kryus <accelrated@gmail.com>
2021-08-23 04:55:37 +0800

    Update 2021-08-22T20:55:34.825Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,25 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+  //Sidebar
+  // Reply-btn
+  $('.reply').click(function (event) {
+    event.preventDefault();
+    $('.replyToggle').toggleClass('d-none');
+    $('.replyContent').toggleClass('d-none');
+    $('.assignmentChange').toggleClass('d-none');
+    $('.replyArea').toggleClass('d-none');
+  });
+  $('.replyCancel').click(function (event) {
+    event.preventDefault();
+    $('.replyToggle').toggleClass('d-none');
+    $('.replyContent').toggleClass('d-none');
+    $('.assignmentChange').toggleClass('d-none');
+    $('.replyArea').toggleClass('d-none');
+  }); // View
+
+  $('.myDropdown').click(function (event) {
+    $('.myDropdownEdit').toggleClass('d-none');
+  });
 });
 //# sourceMappingURL=all.js.map
